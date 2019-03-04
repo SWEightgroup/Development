743174c477b552ca253678441d3697f6dd88fdb0
Gionata Legrottaglie <legrottaglie.gionata@gmail.com>
2019-03-04 16:24:41 +0100

    fix - rediect error page - non completo

---
+++ SpringBootDockerMavenColletta/src/main/resources/precache-manifest.c33e18903acf18deedad8d3bfd775377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f142f1b081b222f19787",
+    "url": "/static/js/main.f142f1b0.chunk.js"
+  },
+  {
+    "revision": "07d31d8c8d9564dfd8f9",
+    "url": "/static/js/2.07d31d8c.chunk.js"
+  },
+  {
+    "revision": "f142f1b081b222f19787",
+    "url": "/static/css/main.9a388894.chunk.css"
+  },
+  {
+    "revision": "5476d796b426a6c6b51d68a9a61e08cf",
+    "url": "/index.html"
+  }
+];
