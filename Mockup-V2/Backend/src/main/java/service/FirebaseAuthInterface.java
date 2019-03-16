cf84ed6de1a285d4bf8380b39d74cee51fcc5129
Gionata Legrottaglie <legrottaglie.gionata@gmail.com>
2019-03-17 00:34:49 +0100

    lool

---
+++ Mockup-V2/Backend/src/main/java/service/FirebaseAuthInterface.java
@@ -0,0 +1,49 @@
+package service;
+
+import java.util.Map;
+
+public interface FirebaseAuthInterface {
+
+	/**
+	 * 
+	 * @param token
+	 * @return
+	 * @throws Exception
+	 */
+	public String getUid(String token) throws Exception;
+	/**
+	 * 
+	 * @param loginData
+	 * @return
+	 * @throws Exception
+	 */
+	public String createUser(String email,String password) throws Exception;
+	/**
+	 * 
+	 * @param email
+	 * @param password
+	 * @return
+	 * @throws Exception
+	 */
+	public String getToken(String email, String password) throws Exception; 
+	/**
+	 * 
+	 * @param token
+	 * @param uid
+	 */
+	public void deleteUser(String token, String uid);
+	/**
+	 * 
+	 * @param token
+	 * @param user
+	 * @return
+	 */
+	public Map<String,Object> updateUser(String token, Map<String,Object> user);
+	/**
+	 * 
+	 * @param uid
+	 * @return
+	 * @throws Exception
+	 */
+	public String createToken(String uid) throws Exception;
+}
