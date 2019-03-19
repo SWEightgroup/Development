d3cb38704edab2d51c004ff505d379ac2dde0691
Francesco Magarotto <magarotto.francesco@gmail.com>
2019-03-19 18:18:32 +0100

    Revert "Edited getAllSenteces[37]"
    This reverts commit 78432a91a78e12345215b2397a35b756d81da83a.

--- Mockup-V2/Backend/src/main/java/it/colletta/repository/TeacherRepository.java
+++ Mockup-V2/Backend/src/main/java/it/colletta/repository/TeacherRepository.java
@@ -1,20 +1,16 @@
 package it.colletta.repository;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.auth.internal.FirebaseCustomAuthToken;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.annotations.NotNull;
@@ -25,28 +21,25 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class TeacherRepository {
 
-    /**
-     * Get all the sentences inserted by the teacher
-     * 
-     * @param teacherId Teacher id
-     * @return Map contains token, uid and user information
-     * @throws ExecutionException
-     * @throws InterruptedException
-     * @throws Exception            Exception.
-     */
-    public Map<String, Object> getAllSentences(@NotNull String teacherId)
-            throws InterruptedException, ExecutionException {
-        DocumentSnapshot document = 
-        FirestoreClient.getFirestore().collection("users").document("teacherId").get().get();
-        Map<String, Object> mapOfPhrases = new HashMap<String, Object>();
-        if(document.exists()) {
-            Map<String, Object> mapOfObj = document.getData();
-            String documentReferences = (String) mapOfObj.get("exercises");
-            
+   /**
+   * Get all the sentences inserted by the teacher 
+   * 
+   * @param  teacherId Teacher id
+   * @return Map contains token, uid and user information
+   * @throws Exception Exception.
+   */
+    public Map<String, Object> getAllSentences(@NotNull String teacherId) {
+        ApiFuture<QuerySnapshot> exercisesQuery = 
+            FirestoreClient.getFirestore().collection("exercises").whereArrayContains("author", teacherId).get();
+        ApiFuture<QuerySnapshot> correctionsQuery = 
+            FirestoreClient.getFirestore().collection("corrections").whereArrayContains("author", teacherId).get();
+        List<QueryDocumentSnapshot> documents = exercisesQuery.get().getDocuments();
+        for(QueryDocumentSnapshot document : documents)   {
+            document.getClass(TeacherModel.class);
         }
         
-        
-        return mapOfPhrases;
+        Map<String, Object> mapOfPhrases = new HashMap<String, Object>();
+
     }
 
     /**
