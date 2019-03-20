f03840c343550570bb6537cda52a188f7fa09c67
Francesco <magarotto.francesco@gmail.com>
2019-03-20 23:08:17 +0100

    Insegnante trovati un lavoro serio

--- Mockup-V2/Backend/src/main/java/it/colletta/repository/TeacherRepository.java
+++ Mockup-V2/Backend/src/main/java/it/colletta/repository/TeacherRepository.java
@@ -1,5 +1,7 @@
 package it.colletta.repository;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.firebase.cloud.FirestoreClient;
@@ -9,6 +11,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
+import javax.print.Doc;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,40 +26,53 @@ public class TeacherRepository {
    * @return Map contains token, uid and user information
    * @throws ExecutionException
    * @throws InterruptedException
-   * @throws Exception Exception.
+   * @throws Exception            Exception.
    */
-  public Object getAllSentences(@NotNull String teacherId)
-      throws InterruptedException, ExecutionException {
-    DocumentSnapshot document =
-        FirestoreClient.getFirestore().collection("users").document(teacherId).get().get();
-    Map<String, Object> mapOfPhrases = new HashMap<String, Object>();  //	TODO 
-    String s = null;
-    if (document.exists()) {
-      
+  public Object getAllSentences(@NotNull String teacherId) throws InterruptedException, ExecutionException {
+    ApiFuture<DocumentSnapshot> documentSnapshot = FirestoreClient.getFirestore().collection("users")
+        .document(teacherId).get();
+    DocumentSnapshot teacherDocSnapshot = documentSnapshot.get();
+    Map<String, Object> map = new HashMap<>();
+    Map<String, Object> temporanyMap = null;
     @SuppressWarnings("unchecked")
-	ArrayList<Object> rf = (ArrayList<Object>) document.getData().get("exercises");
-    
-    for(Object item : rf) {
-    	FirestoreClient.getFirestore().document(((DocumentReference)item).getPath()).get().get().get("");
+    ArrayList<Object> listExercises = (ArrayList<Object>) teacherDocSnapshot.getData().get("exercises");
+    for (Object exercise : listExercises) {
+      // DocumentSnapshot solutionDocument = FirestoreClient.getFirestore()
+      // .document(((DocumentReference) exercise).getPath()).get().get();
+      // temporanyMap = solutionDocument.getData();
+      // map.put("testo", temporanyMap.get("phrase").toString());
+      // map.put("soluzione", temporanyMap.get("solution").toString());
+      String path = ((DocumentReference) exercise).getPath();
+      String text = FirestoreClient.getFirestore().document(path).get().get().get("phrase").toString();
+      map.put("dio", text);
     }
-    		
-    return ((DocumentReference)rf.get(0)).getPath();
-      //return rf.getPath();
-    }
-    return null;
-    // return mapOfPhrases;
+    return map;
+    /*
+     * document.Map<String, Object> mapOfPhrases = new HashMap<String, Object>(); //
+     * TODO String s = null; if (document.exists()) {
+     * 
+     * @SuppressWarnings("unchecked") ArrayList<Object> rf = (ArrayList<Object>)
+     * document.getData().get("exercises");
+     * 
+     * for (Object item : rf) {
+     * FirestoreClient.getFirestore().document(((DocumentReference)
+     * item).getPath()).get().get().get(""); }
+     * 
+     * return ((DocumentReference) rf.get(0)).getPath(); // return rf.getPath(); }
+     * return null; // return mapOfPhrases;
+     */
   }
 
   /**
    * A teacher insert a new exercise
    *
-   * @param phrase Teacher id,
-   * @return
-   * @throws
+   * @param phrase Teacher id, @return @throws
    */
   public boolean insertPhrase(@NotNull String phrase, String uid) {
-    // va fatta una query con tutte gli esercizi scritti dall'insegnante, se non è presente
-    // un esercizio con lo stesso testo che si vuole aggiungere allora aggiungo l'esercizio
+    // va fatta una query con tutte gli esercizi scritti dall'insegnante, se non è
+    // presente
+    // un esercizio con lo stesso testo che si vuole aggiungere allora aggiungo
+    // l'esercizio
     return true;
   }
 }
