9af8cf13cd7a572d536f573b9b686fd76132394d
Francesco Magarotto <magarotto.francesco@gmail.com>
2019-03-21 17:05:46 +0100

    TeacherRepo

--- Mockup-V2/Backend/src/main/java/it/colletta/repository/TeacherRepository.java
+++ Mockup-V2/Backend/src/main/java/it/colletta/repository/TeacherRepository.java
@@ -36,7 +36,7 @@ public class TeacherRepository {
     @SuppressWarnings("unchecked")
     ArrayList<Object> phrasesPathList = (ArrayList<Object>) (teacherDocSnapshot.getData()).get("phrases");    // phrases path of the teacher 
     for (Object phrase : phrasesPathList) {
-      Map<String, Object> solutionMap = new HashMap<>(MAXIMUM_SOLUTION);
+      Map<String, Object> solutionMap = new HashMap<>(MAXIMUM_SOLUTION); 
       String pathPhrase = ((DocumentReference) phrase).getPath();           // the phrase position inside the table "phrases"
       String text =
           FirestoreClient.getFirestore().document(pathPhrase).get().get().get("text").toString();   // the text of the phrase 
@@ -65,7 +65,29 @@ public class TeacherRepository {
    * @param teacherId the document identifier of the collection in firebase
    * @throws something 
    */
-  public boolean insertPhrase(@NotNull String phrase, @NotNull String solution, String teacherId) {
+
+  public boolean insertPhrase(@NotNull String teacherId, @NotNull String phrase,
+    @NotNull String solution, @NotNull String freeLingSolution, String alternativeSolution) {
+    // controllare se la frase è già presente 
+    // se non lo è: 
+    List<QueryDocumentSnapshot> phraseWithSolutions = 
+        FirestoreClient.getFirestore().collection("phrases").whereEqualTo("text", phrase).get().get().getDocuments(); // check if the phrases already exist 
+    if(phraseWithSolutions.size() == 1) {   //the phrase already exsist so I have to add only the solutions 
+      DocumentReference documentref = phraseWithSolutions.get(0).getReference();
+      Map<String, Object> iMap = new HashMap<String, Object>(3);
+      Map<String, Object>[] submap = new HashMap<String, Object>(3);
+      iMap.put("author", teacherId);
+      
+      FirestoreClient.getFirestore().document(
+        documentref.getPath()
+        ).collection("solutions").add("");
+    }
+    Map<String, Object> update = new HashMap<>();  
+    ApiFuture<WriteResult> writeResult = 
+        FirestoreClient.getFirestore().collection("users").document(teacherId).get();        
+    DocumentSnapshot teacherDocSnapshot = writeResult.get();
+
+    
 
     return true;
   }
