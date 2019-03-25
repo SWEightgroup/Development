04a508aa1850d6ab4a9f2325b1868f07d78d7bb5
maga97 <mf12996b>
2019-03-25 16:12:27 +0100

    Tolto il java che ciava

--- Mockup-V2/Backend/src/main/java/it/colletta/repository/UserCustomQueryInterface.java
+++
@@ -1,8 +0,0 @@
-package it.colletta.repository;
-
-// interface for custom query on users collections
-
-public interface UserCustomQueryInterface {
-  public void updateActivateFlagOnly(String id);
-
-}
