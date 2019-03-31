d135f4cdd52e8723aab208c404c15eb4ea0b4a4f
Francesco Magarotto <magarotto.francesco@gmail.com>
2019-03-31 19:05:30 +0200

    Sistemati bug

--- Mockup-V2/Backend/src/main/java/it/colletta/model/helper/ExerciseHelper.java
+++ Mockup-V2/Backend/src/main/java/it/colletta/model/helper/ExerciseHelper.java
@@ -1,30 +1,23 @@
-package it.colletta.model;
+package it.colletta.model.helper;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
+import java.util.List;
 
 @Getter
-@Setter
+@NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @ToString
-public class ExerciseModel {
-    @Id
-    private String id;
-    private Long dateExercise;
-    @DBRef
-    private PhraseModel phraseReference;
-    private Boolean visibilty;
-    private Boolean toDo;
-
-    public ExerciseModel() {
-        id = new ObjectId().toHexString();
-        dateExercise = System.currentTimeMillis();
-        phraseReference = new PhraseModel();
-        visibilty = true;
-        toDo = false;
-    }
+public class ExerciseHelper {
+    private List<String> assignedUsersIds;
+    private String phraseText;
+    private String mainSolution;
+    private String alternativeSolution;
+    private Boolean visibility;
+    private String author;
+    private Long date;
+    private String language;
 }
