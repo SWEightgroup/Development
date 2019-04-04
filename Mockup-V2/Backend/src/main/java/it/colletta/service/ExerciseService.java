a6d0c678a28858eebebfb43c41a177d02ec8918b
fraccorzo <francesco.corti@studenti.unipd.it>
2019-04-04 19:53:55 +0200

    Sistemato

--- Mockup-V2/Backend/src/main/java/it/colletta/service/ExerciseService.java
+++ Mockup-V2/Backend/src/main/java/it/colletta/service/ExerciseService.java
@@ -17,50 +17,50 @@ import java.util.Optional;
 @Service
 public class ExerciseService {
 
-    @Autowired
-    private ExerciseRepository exerciseRepository;
+  @Autowired
+  private ExerciseRepository exerciseRepository;
 
-    @Autowired
-    private PhraseService phraseService;
+  @Autowired
+  private PhraseService phraseService;
 
-    @Autowired
-    private UserService userService;
+  @Autowired
+  private UserService userService;
 
-    public Optional<ExerciseModel> findById(String id) {
-        return exerciseRepository.findById(id);
-        // controllo su id, in caso exception
+  public Optional<ExerciseModel> findById(String id) {
+    return exerciseRepository.findById(id);
+    // controllo su id, in caso exception
+  }
+
+  public void modifyExerciseAuthorName(UserModel newUserData, String token) {
+    UserModel oldUser = userService.findByEmail(newUserData.getUsername());
+    Optional<String> firstName = Optional.ofNullable(newUserData.getFirstName());
+    Optional<String> lastName = Optional.ofNullable(newUserData.getLastName());
+    if (firstName.isPresent() && !firstName.get().equals(oldUser.getFirstName())
+            || lastName.isPresent() && !lastName.get().equals(oldUser.getLastName())) {
+      String authorName = newUserData.getFirstName() + " " + newUserData.getLastName();
+      exerciseRepository.modifyAuthorName(authorName, oldUser.getId());
+    }
+  }
+
+  public ExerciseModel insertExercise(ExerciseHelper exercise) {
+
+    SolutionModel mainSolution = SolutionModel.builder().reliability(0)
+            .authorId(exercise.getAuthor()).solutionText(exercise.getMainSolution()).build();
+
+    SolutionModel alternativeSolution = null;
+    if (exercise != null && !exercise.getAlternativeSolution().isEmpty()) {
+      alternativeSolution =
+              SolutionModel.builder().reliability(0).authorId(exercise.getAuthor())
+                      .solutionText(exercise.getAlternativeSolution()).build();
     }
 
-    public void modifyExerciseAuthorName(UserModel newUserData, String token) {
-        UserModel oldUser = userService.findByEmail(newUserData.getUsername());
-        Optional<String> firstName = Optional.ofNullable(newUserData.getFirstName());
-        Optional<String> lastName = Optional.ofNullable(newUserData.getLastName());
-        if (firstName.isPresent() && !firstName.get().equals(oldUser.getFirstName())
-                || lastName.isPresent() && !lastName.get().equals(oldUser.getLastName())) {
-            String authorName = newUserData.getFirstName() + " " + newUserData.getLastName();
-            exerciseRepository.modifyAuthorName(authorName, oldUser.getId());
-        }
-    }
+    PhraseModel phrase = PhraseModel.builder().language(exercise.getLanguage())
+            .datePhrase(System.currentTimeMillis()).phraseText(exercise.getPhraseText())
+            .build();
 
-    public ExerciseModel insertExercise(ExerciseHelper exercise) {
-
-        SolutionModel mainSolution = SolutionModel.builder().reliability(0)
-                .authorId(exercise.getAuthor()).solutionText(exercise.getMainSolution()).build();
-
-        SolutionModel alternativeSolution = null;
-        if (exercise != null && !exercise.getAlternativeSolution().isEmpty()) {
-            alternativeSolution =
-                    SolutionModel.builder().reliability(0).authorId(exercise.getAuthor())
-                            .solutionText(exercise.getAlternativeSolution()).build();
-        }
-
-        PhraseModel phrase = PhraseModel.builder().language(exercise.getLanguage())
-                .datePhrase(System.currentTimeMillis()).phraseText(exercise.getPhraseText())
-                .build();
-
-        phrase.addSolution(mainSolution);
-        phrase.addSolution(alternativeSolution);
-        phrase = phraseService.insertPhrase(phrase);
+    phrase.addSolution(mainSolution);
+    phrase.addSolution(alternativeSolution);
+    phrase = phraseService.insertPhrase(phrase);
 
         Optional<UserModel> user = userService.findById(exercise.getAuthor());
         String authorName =
@@ -81,12 +81,12 @@ public class ExerciseService {
 
 
 
-    public void deleteExercise(String exerciseId) {
-        Optional<ExerciseModel> exerciseModelOptional = exerciseRepository.findById(exerciseId);
-        if (exerciseModelOptional.isPresent()) {
-            ExerciseModel exerciseModel = exerciseModelOptional.get();
-            exerciseRepository.delete(exerciseModel);
-        }
-        // else exception
+  public void deleteExercise(String exerciseId) {
+    Optional<ExerciseModel> exerciseModelOptional = exerciseRepository.findById(exerciseId);
+    if (exerciseModelOptional.isPresent()) {
+      ExerciseModel exerciseModel = exerciseModelOptional.get();
+      exerciseRepository.delete(exerciseModel);
     }
+    // else exception
+  }
 }
