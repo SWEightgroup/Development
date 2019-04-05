d4a55e3ee42b66d8bf2c6e3cde45eea91378383c
sebacaccaro <sebastianocaccaro@gmail.com>
2019-04-05 18:27:10 +0200

    predisposte action amministrare

---
+++ Mockup-V2/Frontend/src/reducers/AdminReducer.js
@@ -0,0 +1,18 @@
+const initState = {
+  devList: []
+};
+
+const AdminReducer = (state = initState, action) => {
+  console.log(action);
+  switch (action.type) {
+    case 'UPDATE_USER_LIST':
+      return {
+        ...state,
+        devList: action.payload
+      };
+    default:
+      return { ...state, loader: false };
+  }
+};
+
+export default AdminReducer;
