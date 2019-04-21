f2d31f2dcd38d665e503e570d390c91487f75301
sebacaccaro <sebastianocaccaro@gmail.com>
2019-04-21 20:43:49 +0200

    Caricati test sul component user

---
+++ Mockup-V2/Frontend/src/test/components/User.test.jsx
@@ -0,0 +1,65 @@
+import expect from 'expect';
+import React from 'react';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import User from '../../view/components/User';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Testing User component', () => {
+  it('render all user info', () => {
+    const props = {
+      firstName: 'TestName',
+      lastName: 'TestLastName',
+      username: 'TestEmail',
+      language: 'it',
+      btAction: jest.fn(),
+      role: 'ROLE_STUDENT',
+      id: '12345456667'
+    };
+
+    const wrapper = shallow(<User {...props} />);
+    expect(wrapper.find('div.widget-heading').length).toBe(1);
+
+    expect(
+      wrapper.contains(
+        <div className="widget-heading">TestName TestLastName</div>
+      )
+    ).toEqual(true);
+
+    expect(
+      wrapper.contains(<td className="text-center">TestEmail</td>)
+    ).toEqual(true);
+
+    expect(wrapper.contains(<td className="text-center">Studente</td>)).toEqual(
+      true
+    );
+  });
+
+  it('function is called when button is pressed', () => {
+    const btAction = jest.fn();
+
+    const props = {
+      firstName: 'TestName',
+      lastName: 'TestLastName',
+      username: 'TestEmail',
+      language: 'it',
+      btAction,
+      role: 'ROLE_STUDENT',
+      id: '12345456667'
+    };
+
+    const wrapper = shallow(<User {...props} />);
+    const button = wrapper.find('button');
+    expect(button.length).toBe(1); // it finds it alright
+    button.simulate('click');
+    expect(btAction).toHaveBeenCalled();
+  });
+
+  /*
+  it('save button is labeled "Saving..." when boolProp is true', () => {
+    const wrapper = setup(true);
+    expect(wrapper.find('input').props().value).toBe('Saving...');
+  }); */
+});
