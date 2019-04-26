3427878eea741c15b5c01d14b03b418314b1be35
Enrico <enrico_97@hotmail.it>
2019-04-26 14:33:30 +0200

    preauthorize funzionante

--- Mockup-V2/Backend/src/main/java/it/colletta/controller/ExerciseController.java
+++ Mockup-V2/Backend/src/main/java/it/colletta/controller/ExerciseController.java
@@ -14,186 +14,179 @@ import it.colletta.model.helper.ExerciseHelper;
 import it.colletta.security.ParseJwt;
 import it.colletta.service.ExerciseService;
 import it.colletta.service.SolutionService;
-import java.io.IOException;
-import java.security.Principal;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.security.RolesAllowed;
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/exercises")
 public class ExerciseController {
 
-  private ExerciseService exerciseService;
-  private SolutionService solutionService;
+    private ExerciseService exerciseService;
+    private SolutionService solutionService;
 
-  @Autowired
-  public ExerciseController(ExerciseService exerciseService,
-      SolutionService solutionService) {
-    this.exerciseService = exerciseService;
-    this.solutionService = solutionService;
-  }
-
-  @RequestMapping(value = "/done", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< ? > allExercisesDone(@RequestHeader("Authorization") String token,
-      @PageableDefault(value = 4) Pageable pageable,
-      PagedResourcesAssembler< ExerciseModel > assembler) {
-    String id = ParseJwt.getIdFromJwt(token);
-
-    Page< ExerciseModel > exercisesDone = exerciseService.getAllDoneBySudentId(pageable, id);
-    PagedResources< ? > resources = assembler
-        .toResource(exercisesDone, new ExerciseResourceAssembler("/done-alt"));
-    return new ResponseEntity<>(resources, HttpStatus.OK);
-  }
-
-  /**
-   * Returns all the exercises added by the requesting teacher.
-   *
-   * @param token User token
-   * @param pageable pageable
-   * @param assembler assembler
-   * @return List of exercises
-   */
-  @RequestMapping(value = "/added", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< ? > allAddedExercises(@RequestHeader("Authorization") String token,
-      @PageableDefault(value = 4) Pageable pageable,
-      PagedResourcesAssembler< ExerciseModel > assembler) {
-    String id = ParseJwt.getIdFromJwt(token);
-
-    Page< ExerciseModel > exercisesDone = exerciseService.getAllAddedByTeacherId(pageable, id);
-    PagedResources< ? > resources = assembler
-        .toResource(exercisesDone, new ExerciseResourceAssembler("/added-alt"));
-    return new ResponseEntity<>(resources, HttpStatus.OK);
-  }
-
-  @RequestMapping(value = "/todo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< ? > allExercisesToDo(@RequestHeader("Authorization") String token,
-      @PageableDefault(value = 4) Pageable pageable,
-      PagedResourcesAssembler< ExerciseModel > assembler) {
-    String id = ParseJwt.getIdFromJwt(token);
-    Page< ExerciseModel > exercisesToDo = exerciseService.getAllToDoByStudentId(pageable, id);
-    PagedResources< ? > resources = assembler
-        .toResource(exercisesToDo, new ExerciseResourceAssembler("/todo-alt"));
-    return new ResponseEntity<>(resources, HttpStatus.OK);
-  }
-
-  /**
-   * @param exercise the exercise which needs to be inserted in the database
-   * @return A new ResponseEntity that contains the phrase.
-   */
-  //@RolesAllowed("TEACHER")
-  @Secured("TEACHER")
-  @RequestMapping(value = "/insert-exercise", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< ExerciseModel > insertExercise(@RequestBody ExerciseHelper exercise) {
-    try {
-      ExerciseModel exerciseModel = exerciseService.insertExercise(exercise);
-
-      return new ResponseEntity<  >(exerciseModel, HttpStatus.OK);
-    } catch (Exception error) {
-      error.printStackTrace();
-      return new ResponseEntity<  >(HttpStatus.BAD_REQUEST);
+    @Autowired
+    public ExerciseController(ExerciseService exerciseService,
+                              SolutionService solutionService) {
+        this.exerciseService = exerciseService;
+        this.solutionService = solutionService;
     }
-  }
 
-  /**
-   * @param token User's token
-   * @param exercise exercise the exercise which needs to be inserted in the database
-   * @return A new ResponseEntity that contains the phrase.
-   */
-  @RequestMapping(value = "/student/insert-free-exercise", method = RequestMethod.POST,
-      produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< ExerciseModel > insertFreeExercise(
-      @RequestHeader("Authorization") String token,
-      @RequestBody ExerciseHelper exercise) {
-    try {
-      ExerciseModel exerciseModel;
-      exerciseModel = exerciseService
-          .insertFreeExercise(exercise, ParseJwt.getIdFromJwt(token));
-      return new ResponseEntity<  >(exerciseModel, HttpStatus.OK);
-    } catch (Exception error) {
-      error.printStackTrace();
-      return new ResponseEntity<  >(HttpStatus.BAD_REQUEST);
-    }
-  }
+    @RequestMapping(value = "/done", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> allExercisesDone(@RequestHeader("Authorization") String token,
+                                              @PageableDefault(value = 4) Pageable pageable,
+                                              PagedResourcesAssembler<ExerciseModel> assembler) {
+        String id = ParseJwt.getIdFromJwt(token);
 
-  /**
-   * @param token the unique token of the user, in this case a student
-   * @param correctionHelper contains the unique id of the exercise and the solution that was
-   * written by the student
-   * @return the teacher solution of the exercise.
-   */
-  @RequestMapping(value = "/student/do", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< SolutionModel > doExercise(@RequestHeader("Authorization") String token,
-      @RequestBody CorrectionHelper correctionHelper) {
-    try {
-      SolutionModel insertedSolution = exerciseService
-          .doExercise(correctionHelper, ParseJwt.getIdFromJwt(token));
-      return new ResponseEntity<>(insertedSolution, HttpStatus.OK);
-    } catch (Exception error) {
-      error.printStackTrace();
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        Page<ExerciseModel> exercisesDone = exerciseService.getAllDoneBySudentId(pageable, id);
+        PagedResources<?> resources = assembler
+                .toResource(exercisesDone, new ExerciseResourceAssembler("/done-alt"));
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
-  }
 
-  /**
-   * @param stringObj the text which has to be analyzed by freeling as map
-   * @param studentToken the unique token of the user
-   * @return A SolutionModel with the analyzed sentence or empty if the service is unavailable.
-   */
-  @RequestMapping(value = "/automatic-solution", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< SolutionModel > getCorrection(@RequestBody Map< String, String > stringObj,
-      @RequestHeader("Authorization") String studentToken) {
-    try {
-      SolutionModel solution = solutionService.getAutomaticCorrection(stringObj.get("text"));
-      return new ResponseEntity< SolutionModel >(solution, HttpStatus.OK);
-    } catch (IOException error) {
-      error.printStackTrace();
-      return new ResponseEntity< SolutionModel >(new SolutionModel(),
-          HttpStatus.SERVICE_UNAVAILABLE);
-    }
-  }
+    /**
+     * Returns all the exercises added by the requesting teacher.
+     *
+     * @param token     User token
+     * @param pageable  pageable
+     * @param assembler assembler
+     * @return List of exercises
+     */
+    @RequestMapping(value = "/added", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> allAddedExercises(@RequestHeader("Authorization") String token,
+                                               @PageableDefault(value = 4) Pageable pageable,
+                                               PagedResourcesAssembler<ExerciseModel> assembler) {
+        String id = ParseJwt.getIdFromJwt(token);
 
-  @RequestMapping(value = "/public", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity< ? > getPublicExercises(@PageableDefault(value = 4) Pageable pageable,
-      PagedResourcesAssembler< ExerciseModel > assembler,
-      @RequestHeader("Authorization") String studentToken) {
-    try {
-      Page< ExerciseModel > exercisesToDo = exerciseService.getAllPublicExercises(pageable, ParseJwt.getIdFromJwt(studentToken));
-      PagedResources< ? > resources = assembler
-          .toResource(exercisesToDo, new ExerciseResourceAssembler("/public-exercise"));
-      return new ResponseEntity<>(resources, HttpStatus.OK);
+        Page<ExerciseModel> exercisesDone = exerciseService.getAllAddedByTeacherId(pageable, id);
+        PagedResources<?> resources = assembler
+                .toResource(exercisesDone, new ExerciseResourceAssembler("/added-alt"));
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
-    catch(Exception e) {
-      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+    @RequestMapping(value = "/todo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> allExercisesToDo(@RequestHeader("Authorization") String token,
+                                              @PageableDefault(value = 4) Pageable pageable,
+                                              PagedResourcesAssembler<ExerciseModel> assembler) {
+        String id = ParseJwt.getIdFromJwt(token);
+        Page<ExerciseModel> exercisesToDo = exerciseService.getAllToDoByStudentId(pageable, id);
+        PagedResources<?> resources = assembler
+                .toResource(exercisesToDo, new ExerciseResourceAssembler("/todo-alt"));
+        return new ResponseEntity<>(resources, HttpStatus.OK);
     }
-  }
-  /**
-   * @param exerciseId the unique id of the exercise
-   * @param jwtToken   the token of the theacher who is going to delete a exercise
-   * @return ccc. @RequestMapping( value = "/exercises/delete/{exerciseid}",
-   *         method = RequestMethod.DELETE, produces =
-   *         MediaType.APPLICATION_JSON_VALUE) public ResponseEntity<Object>
-   *         deleteExercise( @RequestParam("exerciseid") String
-   *         exerciseId, @RequestHeader("Authorization") String jwtToken) { String
-   *         userId = ParseJWT.getIdFromJwt(jwtToken);
-   *         userService.deleteExerciseAssigned(exerciseId, userId);
-   *         exerciseService.deleteExercise(exerciseId); return new
-   *         ResponseEntity<>(HttpStatus.OK); }
-   */
+
+    /**
+     * @param exercise the exercise which needs to be inserted in the database
+     * @return A new ResponseEntity that contains the phrase.
+     */
+    //@RolesAllowed("TEACHER")
+    //@Secured("ROLE_TEACHER")
+    @PreAuthorize("hasRole('TEACHER')")
+    @RequestMapping(value = "/insert-exercise", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ExerciseModel> insertExercise(@RequestBody ExerciseHelper exercise) {
+        try {
+            ExerciseModel exerciseModel = exerciseService.insertExercise(exercise);
+
+            return new ResponseEntity<>(exerciseModel, HttpStatus.OK);
+        } catch (Exception error) {
+            error.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * @param token    User's token
+     * @param exercise exercise the exercise which needs to be inserted in the database
+     * @return A new ResponseEntity that contains the phrase.
+     */
+    @RequestMapping(value = "/student/insert-free-exercise", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ExerciseModel> insertFreeExercise(
+            @RequestHeader("Authorization") String token,
+            @RequestBody ExerciseHelper exercise) {
+        try {
+            ExerciseModel exerciseModel;
+            exerciseModel = exerciseService
+                    .insertFreeExercise(exercise, ParseJwt.getIdFromJwt(token));
+            return new ResponseEntity<>(exerciseModel, HttpStatus.OK);
+        } catch (Exception error) {
+            error.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * @param token            the unique token of the user, in this case a student
+     * @param correctionHelper contains the unique id of the exercise and the solution that was
+     *                         written by the student
+     * @return the teacher solution of the exercise.
+     */
+    @RequestMapping(value = "/student/do", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SolutionModel> doExercise(@RequestHeader("Authorization") String token,
+                                                    @RequestBody CorrectionHelper correctionHelper) {
+        try {
+            SolutionModel insertedSolution = exerciseService
+                    .doExercise(correctionHelper, ParseJwt.getIdFromJwt(token));
+            return new ResponseEntity<>(insertedSolution, HttpStatus.OK);
+        } catch (Exception error) {
+            error.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * @param stringObj    the text which has to be analyzed by freeling as map
+     * @param studentToken the unique token of the user
+     * @return A SolutionModel with the analyzed sentence or empty if the service is unavailable.
+     */
+    @RequestMapping(value = "/automatic-solution", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SolutionModel> getCorrection(@RequestBody Map<String, String> stringObj,
+                                                       @RequestHeader("Authorization") String studentToken) {
+        try {
+            SolutionModel solution = solutionService.getAutomaticCorrection(stringObj.get("text"));
+            return new ResponseEntity<SolutionModel>(solution, HttpStatus.OK);
+        } catch (IOException error) {
+            error.printStackTrace();
+            return new ResponseEntity<SolutionModel>(new SolutionModel(),
+                    HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    @RequestMapping(value = "/public", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getPublicExercises(@PageableDefault(value = 4) Pageable pageable,
+                                                PagedResourcesAssembler<ExerciseModel> assembler,
+                                                @RequestHeader("Authorization") String studentToken) {
+        try {
+            Page<ExerciseModel> exercisesToDo = exerciseService.getAllPublicExercises(pageable, ParseJwt.getIdFromJwt(studentToken));
+            PagedResources<?> resources = assembler
+                    .toResource(exercisesToDo, new ExerciseResourceAssembler("/public-exercise"));
+            return new ResponseEntity<>(resources, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    /**
+     * @param exerciseId the unique id of the exercise
+     * @param jwtToken   the token of the theacher who is going to delete a exercise
+     * @return ccc. @RequestMapping( value = "/exercises/delete/{exerciseid}",
+     *         method = RequestMethod.DELETE, produces =
+     *         MediaType.APPLICATION_JSON_VALUE) public ResponseEntity<Object>
+     *         deleteExercise( @RequestParam("exerciseid") String
+     *         exerciseId, @RequestHeader("Authorization") String jwtToken) { String
+     *         userId = ParseJWT.getIdFromJwt(jwtToken);
+     *         userService.deleteExerciseAssigned(exerciseId, userId);
+     *         exerciseService.deleteExercise(exerciseId); return new
+     *         ResponseEntity<>(HttpStatus.OK); }
+     */
 }
--- Mockup-V2/Backend/src/main/java/it/colletta/security/WebSecurity.java
+++ Mockup-V2/Backend/src/main/java/it/colletta/security/WebSecurity.java
@@ -1,12 +1,11 @@
 package it.colletta.security;
 
-import static it.colletta.security.SecurityConstants.SIGN_UP_URL;
-
 import com.google.common.collect.ImmutableList;
 import it.colletta.repository.user.UsersRepository;
 import it.colletta.service.user.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,81 +13,93 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import static it.colletta.security.SecurityConstants.SIGN_UP_URL;
+
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  UsersRepository usersRepository;
-  private UserDetailsServiceImpl userDetailsService;
-  private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    UsersRepository usersRepository;
 
-  /**
-   * Constructor.
-   *
-   * @param userDetailsService User details
-   * @param passwordEncoder Encoder
-   */
-  public WebSecurity(UserDetailsServiceImpl userDetailsService,
-      BCryptPasswordEncoder passwordEncoder) {
-    this.userDetailsService = userDetailsService;
-    this.passwordEncoder = passwordEncoder;
-  }
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
-  /**
-   * Configure the security for Spring.
-   *
-   * @param http the http security object
-   */
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().cors().and().authorizeRequests().antMatchers(HttpMethod.POST, SIGN_UP_URL)
-        .permitAll()
-        //.antMatchers(HttpMethod.GET, "/count").permitAll()
-        .antMatchers("/*", "/resources/**", "/resources/static/**", "/resources/assets/**",
-            "/resources/static/static/**")
-        .anonymous().anyRequest().authenticated().and()
-        .addFilter(new JwtAuthenticationFilter(authenticationManager(), usersRepository))
-        .addFilter(new JwtAuthorizationFilter(authenticationManager()))
-        // this disables session creation on Spring Security
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-  }
+    private BCryptPasswordEncoder passwordEncoder;
 
-  /**
-   *
-   */
-  @Override
-  public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-  }
+    /**
+     * Constructor.
+     *
+     * @param userDetailsService User details
+     * @param passwordEncoder    Encoder
+     */
+    public WebSecurity(UserDetailsServiceImpl userDetailsService,
+                       BCryptPasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-  @Override
-  public void configure(org.springframework.security.config.annotation.web.builders.WebSecurity web)
-      throws Exception {
-    web.ignoring().antMatchers("/resources/**", "/static/**", "/assets/**",
-        "/resources/static/static/**");
-  }
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new UserDetailsServiceImpl(usersRepository);
+    }
 
-  /**
-   * Define the registerCorsConfiguration.
-   */
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    final CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(ImmutableList.of("*"));
-    configuration.setAllowedMethods(
-        ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-    configuration.setAllowCredentials(true);
-    configuration.setAllowedHeaders(ImmutableList.of("*"));
-    configuration.setExposedHeaders(ImmutableList.of("X-Auth-Token", "Authorization",
-        "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
+    /**
+     * Configure the security for Spring.
+     *
+     * @param http the http security object
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and().authorizeRequests().antMatchers(HttpMethod.POST, SIGN_UP_URL)
+                .permitAll()
+                //.antMatchers(HttpMethod.GET, "/count").permitAll()
+                .antMatchers("/*", "/resources/**", "/resources/static/**", "/resources/assets/**",
+                        "/resources/static/static/**")
+                .anonymous().anyRequest().authenticated().and()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(), usersRepository))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userDetailsService))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(org.springframework.security.config.annotation.web.builders.WebSecurity web)
+            throws Exception {
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/assets/**",
+                "/resources/static/static/**");
+    }
+
+    /**
+     * Define the registerCorsConfiguration.
+     */
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(
+                ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(ImmutableList.of("*"));
+        configuration.setExposedHeaders(ImmutableList.of("X-Auth-Token", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
